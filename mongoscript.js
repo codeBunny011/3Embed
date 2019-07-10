3240144250f2932bc2d831a44bda00fd9b71de34
Shivansh Mudgil <codeBunny011@users.noreply.github.com>
2019-07-11 01:49:00 +0530

    Update mongoscript.js

--- mongoscript.js
+++ mongoscript.js
@@ -1,16 +1,21 @@
-use admin
+use admin;
+db.system.users.remove({});
+db.system.version.remove({});
+db.system.version.insert({ "_id" : "authSchema", "currentVersion" : 3 });
+use admin;
 db.createUser(
   {
 	user: "root_admin",
-	pwd: "LkqbVNMZ9hxxBehG",
+	pwd: "sussitDev",
 	roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
   }
 );
-use sussitDev
-db.sussitDev.insert( { item: "test", qty: 15 } )
+use sussitDev;
 db.createUser(
   {
 	user: "root_DB",
 	pwd: "sussitDev",
 	roles: [ { role: "dbAdmin", db: "sussitDev" }, { role: "readWrite", db: "sussitDev" } ]
-  });
+  }
+);
+exit;
